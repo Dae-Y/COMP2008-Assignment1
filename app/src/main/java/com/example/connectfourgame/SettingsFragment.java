615fbf7df293063d69fe40a0572a7d55abd6a9da
Aidan C Gom <aidan.c.gom@gmail.com>
2024-09-13 15:49:19 +0800

    Settings fragment done, any bugs encountered pls make a bug report

--- app/src/main/java/com/example/connectfourgame/SettingsFragment.java
+++ app/src/main/java/com/example/connectfourgame/SettingsFragment.java
@@ -1,9 +1,11 @@
 package com.example.connectfourgame;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
@@ -22,6 +24,8 @@ import com.example.connectfourgame.SubSettingFragments.SettingFragmentChangePlay
  *  Author : ACG
  */
 public class SettingsFragment extends Fragment {
+    LinearLayout playerProfileLinearLayout;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,48 +42,73 @@ public class SettingsFragment extends Fragment {
         Button changeGameModeBtn = view.findViewById(R.id.changeGameModeBtn);
         Button changePlayerProfileBtn = view.findViewById(R.id.changePlayerProfileBtn);
 
+        Button player1Btn = view.findViewById(R.id.player1Btn);
+        Button player2Btn = view.findViewById(R.id.player2Btn);
+        player1Btn.setBackgroundColor(Color.parseColor("#6A7BBF"));
+        player2Btn.setBackgroundColor(Color.parseColor("#6A7BBF"));
+
         Button backBtn = view.findViewById(R.id.backBtn);
 
         FrameLayout changeMapSizeFrameLayout = view.findViewById(R.id.gameBoardFrameLayout);
         FrameLayout gameModeFrameLayout = view.findViewById(R.id.gameModeFrameLayout);
-        FrameLayout playerProfileFrameLayout = view.findViewById(R.id.playerProfileFrameLayout);
+        FrameLayout player1FrameLayout = view.findViewById(R.id.player1FrameLayout);
+        FrameLayout player2FrameLayout = view.findViewById(R.id.player2FrameLayout);
 
-        changeGameBoardSizeBtn.setOnClickListener(new View.OnClickListener() {
+        playerProfileLinearLayout = view.findViewById(R.id.changePlayerProfileLinearLayout);
+
+        /* Output Name */
+        viewModel.playerDataArr.observe(getViewLifecycleOwner(), new Observer<PlayerData[]>() {
             @Override
-            public void onClick(View view) {
-                changeMapSizeFrameLayout.setVisibility(View.VISIBLE);
-                gameModeFrameLayout.setVisibility(View.GONE);
-                playerProfileFrameLayout.setVisibility(View.GONE);
-                gameBoardFrameLayout();
+            public void onChanged(PlayerData[] playerData) {
+                PlayerData temp = viewModel.getPlayerData(0);
+                player1Btn.setText(String.valueOf(temp.getPlayerName() + " [Player_1]"));
+                temp = viewModel.getPlayerData(1);
+                if(temp.getPlayerAI()){
+                    player2Btn.setText(String.valueOf(temp.getPlayerName() + " [Player_2[BOT]]"));
+                }else{
+                    player2Btn.setText(String.valueOf(temp.getPlayerName() + " [Player_2]"));
+                }
             }
         });
 
-        changeGameModeBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                gameModeFrameLayout.setVisibility(View.VISIBLE);
-                playerProfileFrameLayout.setVisibility(View.GONE);
-                changeMapSizeFrameLayout.setVisibility(View.GONE);
-                gameModeFrameLayout();
-            }
+        /* SetOnClickListener group */
+        changeGameBoardSizeBtn.setOnClickListener(view16 -> {
+            changeMapSizeFrameLayout.setVisibility(View.VISIBLE);
+            gameModeFrameLayout.setVisibility(View.GONE);
+            playerProfileLinearLayout.setVisibility(View.GONE);
+            gameBoardFrameLayout();
         });
 
-        changePlayerProfileBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                playerProfileFrameLayout.setVisibility(View.VISIBLE);
-                gameModeFrameLayout.setVisibility(View.GONE);
-                changeMapSizeFrameLayout.setVisibility(View.GONE);
-                playerProfileFrameLayout();
-            }
+        changeGameModeBtn.setOnClickListener(view15 -> {
+            gameModeFrameLayout.setVisibility(View.VISIBLE);
+            playerProfileLinearLayout.setVisibility(View.GONE);
+            changeMapSizeFrameLayout.setVisibility(View.GONE);
+            gameModeFrameLayout();
         });
 
-        backBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                viewModel.setFragmentState(0);
-            }
+        changePlayerProfileBtn.setOnClickListener(view14 -> {
+            playerProfileLinearLayout.setVisibility(View.VISIBLE);
+            gameModeFrameLayout.setVisibility(View.GONE);
+            changeMapSizeFrameLayout.setVisibility(View.GONE);
         });
+
+        player1Btn.setOnClickListener(view1 -> {
+            player1FrameLayout.setVisibility(View.VISIBLE);
+            player2FrameLayout.setVisibility(View.GONE);
+            playerProfileFrameLayout("p1", 1);
+            player1Btn.setBackgroundColor(Color.parseColor("#3B4A8B"));
+            player2Btn.setBackgroundColor(Color.parseColor("#6A7BBF"));
+        });
+
+        player2Btn.setOnClickListener(view12 -> {
+            player1FrameLayout.setVisibility(View.GONE);
+            player2FrameLayout.setVisibility(View.VISIBLE);
+            playerProfileFrameLayout("p2", 2);
+            player2Btn.setBackgroundColor(Color.parseColor("#3B4A8B"));
+            player1Btn.setBackgroundColor(Color.parseColor("#6A7BBF"));
+        });
+
+        backBtn.setOnClickListener(view13 -> viewModel.setFragmentState(0));
 
         return view;
     }
@@ -98,10 +127,17 @@ public class SettingsFragment extends Fragment {
         transaction.commit();
     }
 
-    private void playerProfileFrameLayout(){
+    private void playerProfileFrameLayout(String player, int playerNumber){
         Fragment newFragment = new SettingFragmentChangePlayerProfile();
+        Bundle bundle = new Bundle();
+        String frameLayoutId = "player" + playerNumber + "FrameLayout";
+        int res = getResources().getIdentifier(frameLayoutId, "id", getContext().getPackageName());
+
+        bundle.putString("player_key", player);
+        newFragment.setArguments(bundle);
         FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
-        transaction.add(R.id.playerProfileFrameLayout, newFragment);
+
+        transaction.add(res, newFragment);
         transaction.commit();
     }
 }
