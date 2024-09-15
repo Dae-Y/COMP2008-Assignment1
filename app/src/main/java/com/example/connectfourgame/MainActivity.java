87226ab5b251cae05459c3253b27b9c6c24d4098
Aidan C Gom <aidan.c.gom@gmail.com>
2024-09-15 20:37:48 +0800

    Added leaderBoardFragment

--- app/src/main/java/com/example/connectfourgame/MainActivity.java
+++ app/src/main/java/com/example/connectfourgame/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     MainActivityData mainActivityDataViewModel;
     StartFragment startFragment = new StartFragment();
     GameFragment gameFragment = new GameFragment();
+    LeaderBoardFragment leaderBoardFragment = new LeaderBoardFragment();
     SettingsFragment settingsFragment = new SettingsFragment();
 
     FrameLayout topFrameLayout;
@@ -42,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
                 // state 1 and 5 are inter-changeable, to enable the start/restart of a game
                 if(mainActivityDataViewModel.getFragmentState() == 1 || mainActivityDataViewModel.getFragmentState() == 5){
                     topFrameLayout.setVisibility(View.VISIBLE);
-                    //TODO: Levan's part for the top Layout
+                    loadLeaderBoard();
                     loadGameBoard();
                 }
                 if(mainActivityDataViewModel.getFragmentState() == 2){
@@ -84,6 +85,16 @@ public class MainActivity extends AppCompatActivity {
             fm.beginTransaction().add(R.id.bottomFrameLayout, settingsFragment).commit();
         }else{
             fm.beginTransaction().replace(R.id.bottomFrameLayout, settingsFragment).commit();
+        }
+    }
+
+    private void loadLeaderBoard(){
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment frag = fm.findFragmentById(R.id.topFrameLayout);
+        if (frag == null) {
+            fm.beginTransaction().add(R.id.topFrameLayout, leaderBoardFragment).commit();
+        } else {
+            fm.beginTransaction().replace(R.id.topFrameLayout, leaderBoardFragment).commit();
         }
     }
 }
